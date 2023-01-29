e5c504c55bb84b149871f3caef33e44511de96f1
Egor Bukharin <egor.buharin.90@mail.ru>
2023-01-29 21:23:21 +0300

    fin

--- test.go
+++ test.go
@@ -1,180 +1,194 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"time"
+	"net/url"
 	"strconv"
 	"strings"
-	"net/url"
-	"bytes"
+	"sync"
+	"time"
+	"os"
 )
 
+const (
+	testUrl			= "http://185.204.3.165"
+	testEndpoint	= "http://185.204.3.165/question/"
+)
 
-func ft_my_post(r *http.Response, q_id string, data url.Values, ch chan<-string) {
+func sendRequest(r *http.Response, questionID string, data url.Values) {
 
-	req, err := http.NewRequest("POST", "http://185.204.3.165/question/" + q_id, bytes.NewBufferString(data.Encode()))
+	req, err := http.NewRequest(http.MethodPost, testEndpoint+questionID, bytes.NewBufferString(data.Encode()))
 	if err != nil {
 		log.Fatal("Error reading request. ", err)
 	}
-	s_c := r.Header.Get("Set-Cookie")
-	req.Header.Set("Set-Cookie", s_c)
+	setCookie := r.Header.Get("Set-Cookie")
+	req.Header.Set("Set-Cookie", setCookie)
 
 	b := req.Header["Set-Cookie"]
-	cookie:= b[0][0:36]
+	cookie := b[0][0:36]
 	req.Header.Set("Cookie", cookie)
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	
+
 	client := &http.Client{Timeout: time.Second * 1}
 
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Fatal("Error reading response. ", err)
+		log.Fatal("Error reading response: ", err)
 	}
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatal("Error reading body. ", err)
+		log.Fatal("Error reading body: ", err)
 	}
+
 	if string(body) != "404 - not found" {
-		ch <- string(body)
-		//fmt.Println(string(body))
-		fmt.Println(<- ch)
-	} else {
-		return
+		fmt.Println(string(body))
 	}
 }
 
-func ft_my_get(r *http.Response, q_id string, ch chan<-string) {
-	var max_r string
-	var max_s string
-	var name string
+func parseTest(r *http.Response, questionID string) {
+	var (
+		maxRadio  string
+		maxSelect string
+		name      string
+	)
+
 	data := url.Values{}
-	req, err := http.NewRequest("GET", "http://185.204.3.165/question/" + q_id, nil)
+	req, err := http.NewRequest("GET", "http://185.204.3.165/question/"+questionID, nil)
 	if err != nil {
-		log.Fatal("Error reading request. ", err)
+		log.Fatal("Error reading request: ", err)
 	}
-	s_c := r.Header.Get("Set-Cookie")
-	req.Header.Set("Set-Cookie", s_c)
+	setCookie := r.Header.Get("Set-Cookie")
+	req.Header.Set("Set-Cookie", setCookie)
 
 	b := req.Header["Set-Cookie"]
-	cookie:= b[0][0:36]
+	cookie := b[0][0:36]
 	req.Header.Set("Cookie", cookie)
-	
+
 	client := &http.Client{Timeout: time.Second * 1}
 
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Fatal("Error reading response. ", err)
+		log.Fatal("Error reading response: ", err)
 	}
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatal("Error reading body. ", err)
+		log.Fatal("Error reading body: ", err)
 	}
-	if string(body) != "404 - not found" {
-		ch <- string(body)
-		//fmt.Println(string(body))
-		fmt.Println(<-ch)
-	} else {
+
+	if string(body) == "404 - not found" {
 		return
 	}
-	
+	fmt.Println(string(body))
+
 	contain := strings.Index(string(body), "<h2>Answer</h2>")
 	count := 0
-	for flag := 0; flag >= 0; flag++ {
-		//count := 0
-
-		if string(body[contain + 50 + 1 + count]) == "i" && string(body[contain + 50 + 13 + count]) == "t" {
+	for {
+		if string(body[contain+50+1+count]) == "i" && string(body[contain+50+13+count]) == "t" {
 			//fmt.Println(string(body[contain + 50 + 13 + count]))
-			name = string(body[(contain + 50 + 13 + 12 + count) : (contain + 50 + 13 + 12 + 15 + 1 + count)])
+			name = string(body[(contain + 50 + 13 + 12 + count):(contain + 50 + 13 + 12 + 15 + 1 + count)])
 			//fmt.Println(name + " name")//name
 			data.Add(name, "test")
-			//data = ft_my_post(r , q_id, name, "test", data)
-		} else if string(body[contain + 50 + 1 + count]) == "i" && string(body[contain + 50 + 13 + count]) == "r" {
-			p_end := strings.Index(string(body[contain + 50 + 13 + count :]), "</p>")
+			//data = sendRequest(r , questionID, name, "test", data)
+		} else if string(body[contain+50+1+count]) == "i" && string(body[contain+50+13+count]) == "r" {
+			pEnd := strings.Index(string(body[contain+50+13+count:]), "</p>")
 
-			r_t := 0
-			for i := contain + 50 + 13 + count; i < p_end + contain + 50 + 13 + count; i++ {
-				v_i := strings.Index(string(body[i + r_t : p_end + contain + 50 + 13 + count]), "value")
-				if r_t + v_i > r_t {
-					r_t = r_t + v_i
-					ko := 0
+			sumValueIndex := 0
+			for i := contain + 50 + 13 + count; i < pEnd+contain+50+13+count; i++ {
+				valueIndex := strings.Index(string(body[i+sumValueIndex:pEnd+contain+50+13+count]), "value")
+				if sumValueIndex+valueIndex > sumValueIndex {
+					sumValueIndex += valueIndex
+					savePosition := 0
 					for j := 0; j >= 0; j++ {
-						if string(body[i + r_t + 7 + j]) == ">" {
-							ko = j
+						if string(body[i+sumValueIndex+7+j]) == ">" {
+							savePosition = j
 							j = -2
 						}
-					} 
-					val := string(body[i + r_t + 7 : i + r_t + 7 + ko - 1])
-					if len(val) > len(max_r) {
-						max_r = val
+					}
+					val := string(body[i+sumValueIndex+7 : i+sumValueIndex+7+savePosition-1])
+					if len(val) > len(maxRadio) {
+						maxRadio = val
 					}
 				} else {
 					break
 				}
 			}
-			max_i := strings.Index(string(body[contain + 50 + 13 + count :]), max_r)
-			name = string(body[max_i - 25 + contain + 50 + 13 + count : max_i - 9 + contain + 50 + 13 + count])
-			data.Add(name, max_r)
-			max_r = ""
+			maxI := strings.Index(string(body[contain+50+13+count:]), maxRadio)
+			name = string(body[maxI-25+contain+50+13+count : maxI-9+contain+50+13+count])
+			data.Add(name, maxRadio)
+			maxRadio = ""
 
+		} else if string(body[contain+50+1+count]) == "s" {
 
-		} else if string(body[contain + 50 + 1 + count]) == "s" {
-
-			s_end := strings.Index(string(body[contain + 50 + 1 + count :]), "</p>")
+			sEnd := strings.Index(string(body[contain+50+1+count:]), "</p>")
 
 			s := 0
-			for i := contain + 50 + 1 + count; i < s_end + contain + 50 + 1 + count; i++ {
-				sv_i := strings.Index(string(body[i + s: s_end + contain + 50 + 1 + count]), "value")
-				if s + sv_i > s {
-					s = s + sv_i
-					so := 0
+			for i := contain + 50 + 1 + count; i < sEnd+contain+50+1+count; i++ {
+				svI := strings.Index(string(body[i+s:sEnd+contain+50+1+count]), "value")
+				if s+svI > s {
+					s = s + svI
+					position := 0
 					for j := 0; j >= 0; j++ {
-						if string(body[i + s + 7 + j]) == ">" {
-							so = j
-							j = -2
+						if string(body[i+s+7+j]) == ">" {
+							position = j
+							break
 						}
-					} 
-					val := string(body[i + s + 7 : i + s + 7 + so - 1])
-					if len(val) > len(max_s) {
-						max_s = val
 					}
-				} else {
-					break
+					res := string(body[i+s+7 : i+s+7+position-1])
+					if len(res) > len(maxSelect) {
+						maxSelect = res
+					}
+					continue
 				}
+				break
 			}
 
-			name = string(body[contain + 50 + 1 + 13 + count : contain + 50 + 1 + 10 + 19 + count])
+			name = string(body[contain+50+1+13+count : contain+50+1+10+19+count])
 
-			data.Add(name, max_s)
-			max_s = ""
+			data.Add(name, maxSelect)
+			maxSelect = ""
 		}
-		line_end := strings.Index(string(body[contain + 50 + count :]), "\n")
-		if string(body[line_end + contain + 50 + 10 + 2 + 1 + count + 1]) == "b" {
-			flag = -2
-				break
+
+		lineEnd := strings.Index(string(body[contain+50+count:]), "\n")
+		if string(body[lineEnd+contain+50+10+2+1+count+1]) == "b" {
+			break
 		}
-		count = count + line_end + 13
+		count += lineEnd + 13
 	}
-	
-	ft_my_post(r , q_id, data, ch)
+
+	sendRequest(r, questionID, data)
 }
 
-func start_request(r *http.Response, n int) {
-	ch := make(chan string)
+func startTest(r *http.Response, wg *sync.WaitGroup) {
+	defer wg.Done()
 	for i := 1; i <= 10; i++ {
-		go ft_my_get(r, strconv.Itoa(i), ch)
+		parseTest(r, strconv.Itoa(i))
 	}
 }
 
 func main() {
-	n := 6
-	r, _ := http.Get("http://185.204.3.165")
-	start_request(r, n)
-}
+	req, _ := http.Get(testUrl)
 
+	var wg sync.WaitGroup
+
+	workerPool := 1
+
+	if len(os.Args) == 2 {
+		workerPool, _ = strconv.Atoi(os.Args[1])
+	} else if len(os.Args) > 2 {
+		log.Fatal("Error too much args: ")
+	}
+
+	for i := 0; i <= workerPool - 1; i++ {
+		wg.Add(1)
+		go startTest(req, &wg)
+	}
+	wg.Wait()
+}
